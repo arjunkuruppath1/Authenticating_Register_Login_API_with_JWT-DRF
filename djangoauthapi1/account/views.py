c5232044fecf20ec1f49a518eedd126b8bf022d8
Arjun Kuruppath <arjun.k@techversantinfo.com>
2022-07-21 15:19:08 +0530

    Complete Authentication API  register login with JWT in DRF

--- djangoauthapi1/account/views.py
+++ djangoauthapi1/account/views.py
@@ -59,18 +59,17 @@ class UserChangePasswordView(APIView):
     def post(self,request,format=None):
         serializer = UserChangePasswordSerializer(data=request.data,
         context={'user':request.user})
-        if serializer.is_valid(raise_exception=True):
-            return Response({'msg':'Password Changed successfully'},
-            status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
+        serializer.is_valid(raise_exception=True)   #if is not needed exception automatically dealt with 
+        return Response({'msg':'Password Changed successfully'},  #by the default exception handler in REST framework and returns http 404 bad request
+        status=status.HTTP_200_OK)
+        
 
 class SendPasswordResetEmailView(APIView):
     renderer_classes = [UserRenderer]
     def post(self,request,format=None):
         serializer = SendPasswordResetEmailSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            return Response({'msg':'Password Reset link send. Please check your Email'},
+        if serializer.is_valid(raise_exception=True):            
+            return Response({'msg':'Password Reset link send. Please check your Email'},  
             status=status.HTTP_200_OK)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
